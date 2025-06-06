7530b0819a8c40e479a1db7a446ee441e50fad21
Denis Filonov <dennis@newcad.se>
2025-06-06 17:04:31 +0200

    Update package-lock.json and package.json to include @cloudflare/vite-plugin and bump version to 0.3.1; add wrangler.jsonc configuration file for Cloudflare integration; modify vite.config.ts to utilize Cloudflare plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   build: {
     rollupOptions: {
       input: {
